732afc44c7c6a756e8ee6679443757ff73048b53
Shaif Chowdhury <shaif_chowdhury1@baylor.edu>
2023-08-03 09:59:29 -0500

    Added

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,15 +38,15 @@ st.write(
 st.header(
   "How large is the standard deviation for the players in terms of skill or win rates?"
 )
+
+
 fig, ax = plt.subplots()
 df.hist("Overall", ax=ax)
 st.pyplot(fig)
 
-st.write(
-  "Standard Deviation = 6.788352681607474"
-  "Range = 44"
-  "Mean = 65.85204164194401"
-)
+st.write("Standard Deviation = 6.788352681607474")
+st.write("Range = 44")
+st.write("Mean = 65.85204164194401")
 st.write(
   "By figuring out the mean, standard deviation, and range, we can see that the mean and SD is actually not as high as what people think. This is due to the fact that many players are hovering in the 60s-70s in overall score and there not being too much outliers in the dataset to really change the average or distance from the mean. However, we still see a wide range of players due to the fact that the range is pretty high."
 )
@@ -85,19 +85,22 @@ sizes = [intersection_count, df1_unique_count, df2_unique_count]
 colors = ['skyblue', 'lightcoral', 'lightgreen']
 explode = (0.1, 0, 0)  # explode the first slice (Intersection)
 # Plotting the pie chart
-plt.figure(figsize=(8, 8))
-fig = plt.pie(sizes,
-              explode=explode,
-              labels=labels,
-              colors=colors,
-              autopct='%1.1f%%',
-              shadow=True,
-              startangle=140)
-plt.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
-plt.title(
-  'Example of an Intersection between Overall Top Players and Top Players in Different Skills'
-)
+fig, ax = plt.subplots(figsize=(8, 8))
+ax.pie(sizes,
+       explode=explode,
+       labels=labels,
+       colors=colors,
+       autopct='%1.1f%%',
+       shadow=True,
+       startangle=140)
+ax.set_title('Example of an Intersection between Overall Top Players and Top Players in Different Skills')
+ax.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+
+# Display the pie chart in Streamlit
 st.pyplot(fig)
+
+st.write("Make sure you have both Streamlit and Matplotlib installed (pip install streamlit matplotlib) to run the code correctly. Also, replace the sample data (sizes, labels, colors, explode) with your actual data to create the desired pie chart.")
+
 
 st.write(
   "There are two types of skill levels: Skills used during play and scores for how each player plays in a position. For the actual skills, there is a low intersection between the players overall score and skill score - many of which are in the low 10s and maybe even lower. There are still some outliers, with some being higher in the 40s. I believe this is the case as many players are good in their own type of skillsets and I theorize many players in different skill intersections are unique and don't come up as much in different skills. For each position on the field, there is a much higher result, with some going into the 30s but with some still in the early 10s. Another thing that could affect this is where the players usually play on the field. A goalie usually isn't good at scoring and a offensive player usually doesn't block shots from reaching the goal. Due to this, many players will not share all of the skill sets that are avaliable in the dataset."
@@ -123,9 +126,7 @@ plt.title('Players with the Best Potential for Improvement')
 plt.xticks(rotation=45, ha='right')
 plt.tight_layout()
 plt.show()
-st.write(
-  "In order to do this, we first needed to filter out the players that were of low overalls, since there were many with high potential-overall differences since they had a lot of time to play. I did this by ensuring that the data only included players above the rating of 85 so that we only received data from the currently relevant players. This showed that the players with the best potential for improvement included big names such as Pedri, Foden, Vinicius Jr., Haaland, and more."
-)
+st.write("In order to do this, we first needed to filter out the players that were of low overalls, since there were many with high potential-overall differences since they had a lot of time to play. I did this by ensuring that the data only included players above the rating of 85 so that we only received data from the currently relevant players. This showed that the players with the best potential for improvement included big names such as Pedri, Foden, Vinicius Jr., Haaland, and more.")
 
 st.header("How does the age of a player affect their potential?")
 df['Potential-Overall'] = df['Potential'] - df['Overall']
@@ -133,13 +134,19 @@ px.scatter(df,x='Age',  y='Potential-Overall', color="Overall")
 st.write("Oftentimes, as someone’s body ages, they get worse at the sport they play. This is shown through the graph, where young teenagers with time to go have higher potentials for improvement, whereas older players who are due to retire soon have a lower, or even no potential for improvement.")
 
 st.header("How does potential affect the value of the release clause?")
+fig1 = px.scatter(df, x='Potential', y='Release Clause', title = 'The Relationship between Potential and Release Clause',
+                  labels = {'Potential': 'Potential', 'Release Clause' : 'Release Clause Value (in euros)'})
+fig1
+st.write("In this diagram, we can see that, as the potential overall of a player increases, so does the release clause value. This is because clubs apply a higher cost upon their players so that they don't leave, since the clubs want better players to stay and help them win.")
 
-
-#How does rating affect international reputation?           : Scatter
+st.header("How does rating affect international reputation?")
+Scatter2 = df.plot.scatter(x='Overall', y = 'International Reputation')
+st.write("In this diagram, we can see that the international reputation of a player is, on average, higher when a player is rated higher. We can see this through the fact that the players at the lower end of the overall spectrum have no international reputation, but players on the highest end of the spectrum have a very high international reputation.")
 
 #Gabe :
 
 #Who are the Top players based on different Positions ?      : Bar Chart
+st.header("what postions are players of a similar rank most likely to play")
 import pandas as pd
 import matplotlib.pyplot as plt
 
@@ -158,8 +165,11 @@ plt.ylabel('Known As')
 plt.title('Best Position vs. Known As')
 plt.xticks(rotation=20)  # Rotate the x-axis labels if they are too long
 plt.show()
-#How does wage impact Overall ranking?                      :  Scatter
+#How does wage impact Overall ranking?  
+st.header("how doesa the wage of a player impact their over all ranking?")
 px.scatter(df, x='Value(in Euro)', y='Overall', color="Nationality")
 #Is there any relation between Position played and age ?    : Scatter
+st.header("what is the relation between postion played and the age of a player")
+
 px.scatter(df, x='Age', y='Positions Played', color="Overall")
 #Conclusion :
